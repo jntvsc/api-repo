1b3fc54baa59fd60edb970b41e8c0b3be52f0de2
Jonatas Vasconcelos <jonatas.vasconcelos@cajudata.com.br>
2023-02-03 12:33:09 -0300

    criação da home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import { useState } from "react"
+
 function Home() {
-    return <div>Home</div>
+    return (
+        <div>
+            <h2>Home - Navegar</h2>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContdor() {
+        //console.log(contador);
+        setContador(contador + 1);        
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContdor}>+ Contador</button>
+        </div>
+    )
 }
 
 export default Home
