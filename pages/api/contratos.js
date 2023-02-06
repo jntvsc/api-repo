59181b4a48a6c22571de645d4f8965db86960e5e
Jonatas Vasconcelos <jonatas.vasconcelos@cajudata.com.br>
2023-02-06 17:06:32 -0300

    acesso ao MongoDB, passando parametro do contrato

--- pages/api/contratos.js
+++ pages/api/contratos.js
@@ -1,10 +1,14 @@
+import { useRouter } from "next/router"
 import connect from "../../utils/database"
 
 export default async function handler(req, res){
     const {db} = await connect()
    
+    const {query} = req
+    const {id} = query
+    
     const contrato = {
-        numContrato: "1001"
+         numContrato: id
     }
 
     const response = await db.collection("contratos").findOne(contrato)
