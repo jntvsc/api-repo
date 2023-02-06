04f06a337fbc4983250ba2a805193256d4484759
Jonatas Vasconcelos <jonatas.vasconcelos@cajudata.com.br>
2023-02-06 10:28:26 -0300

    adiciona rota /api/tempo de novo

---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+function Sobre() {
+    return (
+        <div>
+            <h2>Navegado para Sobre</h2>
+            
+            <Link href="/">
+                <a>Página Home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Sobre
